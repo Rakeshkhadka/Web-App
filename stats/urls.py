00077deb9d4aee70f9c447c234b405ae6b5af4ad
Rakesh Khadka <rakesh23khadka@gmail.com>
2023-05-14 13:25:38 +0545

    modified urls

--- stats/urls.py
+++ stats/urls.py
@@ -4,7 +4,9 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'weather', SourceDataViewSet, basename='weather')
-router.register(r'weathers/stats', WeatherSummaryViewset, basename='weather-summary')
+# router.register(r'weather/stats', WeatherSummaryViewset, basename='weather-summary')
 urlpatterns = [
+    path('weather/stats/', WeatherSummaryViewset.as_view({'get': 'list'}), name='weather-summary-list'),
+    path('weather/stats/<int:pk>/', WeatherSummaryViewset.as_view({'get': 'retrieve'}), name='weather-summary-detail'),
     path('', include(router.urls)),
 ]
